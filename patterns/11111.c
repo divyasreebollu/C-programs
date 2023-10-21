8742fa16d95b82c20377a0a04a8103fd0cbaba60
divyasreebollu <141166868+divyasreebollu@users.noreply.github.com>
2023-10-22 01:49:03 +0530

    Add files via upload

---
+++ patterns/11111.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=6;i++)
+	{
+		for(j=1;j<=6;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+}
+
+111111
+222222
+333333
+444444
+555555
+666666
+
